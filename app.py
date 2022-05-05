556f8c9ddf659cd40c2da72ed56d8b9e40db3f9b
CassiaFa <fabio.gavancha-cassiano@isen-ouest.yncrea.fr>
2022-05-05 18:34:56 +0200

    Update app

--- app.py
+++ app.py
@@ -63,12 +63,7 @@ def main():
         # else:
         #     st.write('Stopped')
 
-        ctx = webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
-        
-        if ctx.video_processor:
-            ctx.video_processor.threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
-            ctx.video_processor.threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
-
+        webrtc_streamer(key="example", video_processor_factory=VideoProcessor,rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
 if __name__ == "__main__":
     main()
